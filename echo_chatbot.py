0eb67d78b6431861d062eaf9c4cb5aaa91107fe8
WilliamGast <william.gast@outlook.com>
2023-11-26 13:13:01 -0600

    First chatbot

---
+++ echo_chatbot.py
@@ -0,0 +1,7 @@
+print('Hello friend!')
+
+print(' as a simple chatbot I will repeat what you say')
+
+stuff_to_echo = input('I would love to hear what you have to say')
+
+print('You said:' + stuff_to_echo)
